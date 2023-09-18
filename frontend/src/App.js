12d9829b37170edfdf6f250a12a0887504ca7f93
afalade7 <74437233+afalade7@users.noreply.github.com>
2023-09-18 02:42:24 -0400

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './components/WelcomePage';
+import WelcomePage from './components/WelcomePage';
+
+function App() {
+  return (
+    <div className="App">
+    
+      <WelcomePage/>
+    </div>
+  );
+}
+
+export default App;
