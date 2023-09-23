fc05ce7a152a988b2e116c975f44b75df62fe4ba
afalade7 <74437233+afalade7@users.noreply.github.com>
2023-09-23 04:03:44 -0400

    op page

--- frontend/src/components/WelcomePage.js
+++ frontend/src/components/WelcomePage.js
@@ -2,26 +2,24 @@ import { React } from 'react';
 import { Link } from 'react-router-dom';
 
 
-import logo from '../logo.svg';
 import '../styles/WelcomePage.scss';
 import welcomeImage from '../assets/WelocmeImage1.jpg';
 
 function WelcomePage() {
   return (
     <div className="WelcomePage">
-      <div> <h1>Welcome Page</h1></div>
 
-      <div className='nav_section'>
-        <span className='logo'>Recturine</span> <div className="navbar"> <span>Product</span> <span>Resources</span> <span>Careers</span> <span className='pipe'>&#124;</span> <span>Log In</span> <span>Sign Up</span> </div>
+      <div className='welcomeNavSection'>
+        <span className='welcomeLogo'>Recturine</span> <div className="welcomeNavBar"> <span>Product</span> <span>Resources</span> <span>Careers</span> <span className='pipe'>&#124;</span> <span>Log In</span> <span>Sign Up</span> </div>
       </div>
 
-      <div className='body'>
-        <div className="body_left">
+      <div className='welcomeBody'>
+        <div className="welcomeBodyLeft">
           <h1>Enter the chatroom, <br /> to your future career.</h1>
           <p>Linking students with experts</p>
           <button>TRY IT NOW</button>
         </div>
-        <div className="body_right">
+        <div className="welcomeBodyRight">
           <img src={welcomeImage} alt="..." />
         </div>
 
